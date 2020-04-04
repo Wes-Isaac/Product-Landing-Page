789d95b1ca4cb629eb75564f98315713a54bdb78
Wes-Isaac <59416136+Wes-Isaac@users.noreply.github.com>
2020-04-04 21:09:11 +0800

    added smooth scrolling

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#header').style.opacity = 0.9;
+  } else {
+    document.querySelector('#header').style.opacity = 1;
+  }
+});
+ 
+// Smooth Scrolling
+$('#header .nav #nav-bar a, #header .b-nav a' ).on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
